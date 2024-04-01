0c068920e373f4293d775248223e8890389ca300
Fawwaz <fawwaz.chowdhury@googlemail.com>
2024-04-01 18:12:28 +0100

    Update page.jsx

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -24,19 +24,11 @@ const items = [
   {
     id: 3,
     color: "from-violet-300 to-purple-300",
-    title: "Vanilla Book App",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: "https://lama.dev",
-  },
-  {
-    id: 4,
-    color: "from-purple-300 to-red-300",
-    title: "Spotify Music App",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    link: "https://lama.dev",
-  },
+    title: "Admin Dashboard Web App",
+    desc: "This is a Frontend web app dashboard built through ReactJS",
+    img: "",
+    link: "https://gilded-kheer-748bd4.netlify.app/",
+  }
 ];
 
 const PortfolioPage = () => {
